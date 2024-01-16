f9c49e302584cd76443685c4a34851a2c6cb3ed9
Singgih <gokonggokong71@gmail.com>
2024-01-16 12:04:13 +0700

    setup convex and clerk

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -7,8 +7,7 @@ const Heading = () => {
     return (
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
-                Your Ideas, Documents, & Plans. Unified. Welcome to 
-                <span className="underline">Jotion</span>
+                Your Ideas, Documents, & Plans. Unified. Welcome to <span className="underline">Jotion</span>
             </h1>
             <h3 className="text-base sm:text-xl md:text-2xl font-medium">
                 Jotion is the connected workspace where <br />
